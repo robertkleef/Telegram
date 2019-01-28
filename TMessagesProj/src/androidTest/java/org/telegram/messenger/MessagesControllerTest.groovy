85c29045ac44d71c8b8c6e59e74ed81d39cecd34
robert <robertk111@hotmail.com>
2019-01-28 21:23:36 +0100

    First tests

---
+++ TMessagesProj/src/androidTest/java/org/telegram/messenger/MessagesControllerTest.groovy
@@ -0,0 +1,16 @@
+package org.telegram.messenger
+
+class MessagesControllerTest extends groovy.util.GroovyTestCase {
+    void testGetBlockedUsers() {
+
+    }
+
+    void testDeleteUserPhoto() {
+    }
+
+    void testDeleteMessages() {
+    }
+
+    void testDeleteMessages1() {
+    }
+}
